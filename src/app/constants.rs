d6722e5104057eb481ae1f1f5a702c5621227a76
Konsl <82901383+Konsl@users.noreply.github.com>
2026-05-12 10:40:01 +0200

    split app module into multiple files

---
+++ src/app/constants.rs
@@ -0,0 +1,32 @@
+use egui::Color32;
+use egui_plot::MarkerShape;
+
+use crate::game::{ResourceDescriptor, ResourcePurity};
+
+pub fn get_resource_color(resource: ResourceDescriptor) -> Color32 {
+    Color32::from_hex(match resource {
+        ResourceDescriptor::OreIron => "#975f6a",
+        ResourceDescriptor::Coal => "#15008e",
+        ResourceDescriptor::OreCopper => "#9b4c2b",
+        ResourceDescriptor::Stone => "#56452d",
+        ResourceDescriptor::RawQuartz => "#9f6c99",
+        ResourceDescriptor::SAM => "#502e8e",
+        ResourceDescriptor::OreBauxite => "#68392d",
+        ResourceDescriptor::OreGold => "#af9c72",
+        ResourceDescriptor::Sulfur => "#afaa27",
+        ResourceDescriptor::OreUranium => "#357336",
+        ResourceDescriptor::Water => "#4a88ab",
+        ResourceDescriptor::LiquidOil => "#603560",
+        ResourceDescriptor::NitrogenGas => "#7d8089",
+    })
+    .unwrap()
+}
+
+pub fn get_purity_marker(purity: ResourcePurity) -> MarkerShape {
+    match purity {
+        ResourcePurity::Impure => MarkerShape::Up,
+        ResourcePurity::Normal => MarkerShape::Diamond,
+        ResourcePurity::Pure => MarkerShape::Circle,
+    }
+}
+
