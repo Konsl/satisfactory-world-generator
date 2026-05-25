a29b205bfd86521ba989f6811767f74d62579a6a
_nova_tech_ <nova@novatech.build>
2026-05-25 12:52:27 -0400

    Make desktop window bigger by default

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -11,7 +11,7 @@ pub fn run_app() -> eframe::Result {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
+            .with_inner_size([800.0, 600.0])
             .with_min_inner_size([400.0, 300.0]),
         ..Default::default()
     };
